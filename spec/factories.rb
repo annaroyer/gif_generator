37f5bb992c39337b5a7d62938d88d1cdcbe28295
Anna Royer <anna.royer@colorado.edu>
2018-02-10 20:23:05 -0700

    fix username attribute error

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
 
   factory :admin, class: User do
-    username sequence(:username) { |n| "Admin #{n}" }
+    sequence(:username) { |n| "Admin #{n}" }
     password 'Password'
     role 1
   end
