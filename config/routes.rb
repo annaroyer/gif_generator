9e470330594d93fa2f6c944f14faabbbc307cdcf
Anna Royer <anna.royer@colorado.edu>
2018-02-11 14:24:26 -0700

    Change user to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'welcome#index'
-resources :user, only: :new
+resources :users, only: :new
 get 'login', to: 'sessions#new'
 end
