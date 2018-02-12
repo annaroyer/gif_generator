05df6e7f5502062a7f5ca46daec6eac17b3c894b
Anna Royer <anna.royer@colorado.edu>
2018-02-11 18:22:52 -0700

    Add gifs table to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180211030821) do
+ActiveRecord::Schema.define(version: 20180212012113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "gifs", force: :cascade do |t|
+    t.string "category"
+    t.string "image_path"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
