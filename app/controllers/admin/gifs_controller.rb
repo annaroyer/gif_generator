4c20b165a2e0112c72d98f50051ce5d331bdbc9d
Anna Royer <anna.royer@colorado.edu>
2018-02-11 18:18:46 -0700

    Add create method for new gifs

--- app/controllers/admin/gifs_controller.rb
+++ app/controllers/admin/gifs_controller.rb
@@ -1,4 +1,13 @@
 class Admin::GifsController < Admin::BaseController
   def new
   end
+
+  def create
+    gif = Gif.new(category: params[:category], image_path: 'fun_gif.jpg')
+    if gif.save
+      redirect_to gifs_path
+    else
+      render :new
+    end
+  end
 end
