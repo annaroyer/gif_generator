935a6a858765444916851ee91b30459ab820497a
Anna Royer <anna.royer@colorado.edu>
2018-02-12 12:55:22 -0700

    Change user_ud to user_id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,15 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
-      session[:user_ud] = user.id
+      session[:user_id] = user.id
       redirect_to user_path(user)
     else
       render :new
     end
+  end
+
+  def destroy
+    session.destroy
+    redirect_to root_path
   end
 end
