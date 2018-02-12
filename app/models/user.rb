3520eaef49737bcea2f25a69d96067f8a6dae408
Anna Royer <anna.royer@colorado.edu>
2018-02-12 14:53:44 -0700

    Add relationships with favorites and gifs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   validates_presence_of :username, :password
   validates_uniqueness_of :username
   enum role: %w(default admin)
+  has_many :favorites
+  has_many :gifs, through: :favorites
 end
