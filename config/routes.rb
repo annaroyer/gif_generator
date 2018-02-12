ca6aa1f41c679c3c1056abfccf39a25b6f6072d1
Anna Royer <anna.royer@colorado.edu>
2018-02-11 22:42:16 -0700

    Change post sessions route to login url

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   resources :users, only: [:new, :create, :show]
+
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
 
   namespace 'admin' do
     resources :gifs, only: [:new, :create]
