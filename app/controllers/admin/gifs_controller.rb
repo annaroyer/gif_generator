6561e51e5ab63f520078929f2929f641bd9b56b2
Anna Royer <anna.royer@colorado.edu>
2018-02-11 21:36:50 -0700

    Add api call to gifs controller

--- app/controllers/admin/gifs_controller.rb
+++ app/controllers/admin/gifs_controller.rb
@@ -3,11 +3,27 @@ class Admin::GifsController < Admin::BaseController
   end
 
   def create
-    gif = Gif.new(category: params[:category], image_path: 'fun_gif.jpg')
+    gif = Gif.new(gif_params)
     if gif.save
       redirect_to gifs_path
     else
       render :new
     end
+  end
+
+  private
+
+  def gif_data
+    api_client = GiphyClient::DefaultApi.new
+    api_key = "YlojntlcQH81pNtzKCqcAMQMSerO0Ttn"
+    opts = {tag: params[:category]}
+    result = api_client.gifs_random_get(api_key, opts)
+    result.data
+  end
+
+  def gif_params
+    { id: gif_data.id,
+      category: params[:category],
+      image_path: gif_data.fixed_height_small_url }
   end
 end
