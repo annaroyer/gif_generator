97f846c062dc345d1007fab3362236daa3f59d91
Anna Royer <anna.royer@colorado.edu>
2018-02-12 15:27:33 -0700

    Change param to read gif id

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,6 @@
 class FavoritesController < ApplicationController
   def create
-    # binding.pry
-    Favorite.create(gif_id: 1, user: current_user)
+    Favorite.create(gif_id: params[:gif_id], user: current_user)
     redirect_to gifs_path
   end
 end
