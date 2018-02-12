b4c8a53d5383c902cfcc7a7627098e29a75d0518
Anna Royer <anna.royer@colorado.edu>
2018-02-12 14:57:03 -0700

    Add favorites table to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180212012113) do
+ActiveRecord::Schema.define(version: 20180212211903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "gif_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["gif_id"], name: "index_favorites_on_gif_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "gifs", force: :cascade do |t|
     t.string "category"
@@ -30,4 +39,6 @@ ActiveRecord::Schema.define(version: 20180212012113) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "favorites", "gifs"
+  add_foreign_key "favorites", "users"
 end
