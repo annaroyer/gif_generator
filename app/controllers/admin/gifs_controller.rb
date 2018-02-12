c9b54390da33b8c842b26c7736e37186b6dc3c35
Anna Royer <anna.royer@colorado.edu>
2018-02-12 13:42:22 -0700

    Reorganize spec directory

--- app/controllers/admin/gifs_controller.rb
+++ app/controllers/admin/gifs_controller.rb
@@ -3,26 +3,11 @@ class Admin::GifsController < Admin::BaseController
   end
 
   def create
-    gif = Gif.new(gif_params)
+    gif = CreateGif.call(params[:category])
     if gif.save
       redirect_to gifs_path
     else
       render :new
     end
-  end
-
-  private
-
-  def gif_data
-    api_client = GiphyClient::DefaultApi.new
-    api_key = "YlojntlcQH81pNtzKCqcAMQMSerO0Ttn"
-    opts = {tag: params[:category]}
-    result = api_client.gifs_random_get(api_key, opts)
-    result.data
-  end
-
-  def gif_params
-    { category: params[:category],
-      image_path: gif_data.fixed_height_small_url }
   end
 end
