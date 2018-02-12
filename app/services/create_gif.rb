f97e07a077d45a431ecb8534197cb053bc13e842
Anna Royer <anna.royer@colorado.edu>
2018-02-12 14:54:21 -0700

    Change api key to secret

--- app/services/create_gif.rb
+++ app/services/create_gif.rb
@@ -1,7 +1,7 @@
 class CreateGif
   def self.call(category)
     api_instance = GiphyClient::DefaultApi.new
-    key = "YlojntlcQH81pNtzKCqcAMQMSerO0Ttn"
+    key = ENV["giphy_api_key"]
     result = api_instance.gifs_random_get(key, {tag: category})
     Gif.create(category: category, image_path: result.data.fixed_height_small_url)
   end
