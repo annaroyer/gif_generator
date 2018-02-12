fed0618da79fde41af1f932bd1d20c5c9f395481
Anna Royer <anna.royer@colorado.edu>
2018-02-11 22:02:37 -0700

    Add validates single word search

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -1,3 +1,8 @@
 class Gif < ApplicationRecord
   validates_presence_of :image_path, :category
+  validates :category, :length => {
+  :maximum => 1,
+  :tokenizer => lambda { |str| str.scan(/\w+/) },
+  :too_long  => "Please limit your summary to %{count} words"
+}
 end
