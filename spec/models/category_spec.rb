c57018bfb79ca537b9b40f5ba4f5def6ec32233b
Anna Royer <anna.royer@colorado.edu>
2018-02-12 20:41:29 -0700

    Add category model and spec with validations for name and has many gifs relationship

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe Category, type: :model do
+  context 'validations' do
+    it { should validate_presence_of :name }
+  end
+
+  context 'uniqueness' do
+    it { should validate_uniqueness_of :name }
+  end
+
+  context 'relationships' do
+    it { should have_many :gifs }
+  end
+end
