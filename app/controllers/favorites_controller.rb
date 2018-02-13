01a5ec83f0041273a13fae2812741fc29bdebe82
Anna Royer <anna.royer@colorado.edu>
2018-02-12 17:55:47 -0700

    Add destroy action

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,4 +3,10 @@ class FavoritesController < ApplicationController
     Favorite.create(gif_id: params[:gif_id], user: current_user)
     redirect_to gifs_path
   end
+
+  def destroy
+    favorite = Favorite.find(params[:id])
+    favorite.destroy
+    redirect_to user_path(current_user)
+  end
 end
