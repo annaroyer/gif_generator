846f252de2991a9617b1890670424ce55f12c603
Anna Royer <anna.royer@colorado.edu>
2018-02-13 16:11:59 -0700

    Remove unneccessary relationships

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -1,6 +1,6 @@
 class Gif < ApplicationRecord
   validates_presence_of :image_path, :category
   validates_uniqueness_of :image_path
-  has_many :favorites
+  has_many :favorites 
   belongs_to :category
 end
