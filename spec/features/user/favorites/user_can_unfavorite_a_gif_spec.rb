431afe46173b3f19adab3f07aae59bc2f3e11c46
Anna Royer <anna.royer@colorado.edu>
2018-02-12 17:59:33 -0700

    Add spec that user can unfavorite gif - works but test failing

---
+++ spec/features/user/favorites/user_can_unfavorite_a_gif_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'As a user' do
+  context 'when I visit my show page and click unfavorite on a gif' do
+    scenario 'I no longer see that gif on the page' do
+      user = create(:user)
+      favorite = create(:favorite, user: user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit user_path(user)
+      expect(page).to have_xpath("//img[contains(@src,'#{favorite.gif.image_path}')]")
+      expect(page).to have_content(favorite.gif.category)
+
+      click_link 'Unfavorite'
+
+      expect(page).to_not have_xpath("//img[contains(@src,'#{favorite.gif.image_path}')]")
+      expect(page).to_not have_content(favorite.gif.category)
+    end
+  end
+end
