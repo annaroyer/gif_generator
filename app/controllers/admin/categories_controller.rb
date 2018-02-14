c98fb6bb9f3f0855c452f431ddc1af7ddc4efb0e
Anna Royer <anna.royer@colorado.edu>
2018-02-13 17:35:49 -0700

    Add destroy method to categories controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,4 +2,10 @@ class Admin::CategoriesController < Admin::BaseController
   def index
     @categories = Category.order(:name)
   end
+
+  def destroy
+    category = Category.find(params[:id])
+    category.destroy
+    redirect_to admin_categories_path
+  end
 end
