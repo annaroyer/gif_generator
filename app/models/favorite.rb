3a2163a524dccdf16e17fe78bcb0c6aaa808f205
Anna Royer <anna.royer@colorado.edu>
2018-02-13 17:00:08 -0700

    change names

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,5 +2,5 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :gif
   has_one :category, through: :gif
-  validates_uniqueness_of :user, scope: :gif
+  validates_uniqueness_of :user_id, scope: :gif_id
 end
