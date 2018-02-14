b4557f6b8907b7b58973f5f47a29867aaac34f92
Anna Royer <anna.royer@colorado.edu>
2018-02-14 01:18:31 -0700

    Base-level styling

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -2,4 +2,10 @@ class GifsController < ApplicationController
   def index
     @categories = Category.alphabetized
   end
+
+  def update
+    favorite = Favorite.find_by(gif_id: params[:id], user: current_user)
+    favorite.destroy
+    redirect_to gifs_path
+  end
 end
