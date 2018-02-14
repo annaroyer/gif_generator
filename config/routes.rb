2166c867ce4adc4d24f9b74b9d18c43234acd2b0
Anna Royer <anna.royer@colorado.edu>
2018-02-13 17:22:25 -0700

    Add admin categories route

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   namespace 'admin' do
     resources :gifs, only: [:new, :create]
+    resources :categories, only: :index
   end
 
   resources :gifs, only: [:index]
