79c0834d3a131523e6e3bd78d65e09bc4494c668
Anna Royer <anna.royer@colorado.edu>
2018-02-15 02:48:43 -0700

    update redirects

--- app/controllers/admin/gifs_controller.rb
+++ app/controllers/admin/gifs_controller.rb
@@ -3,9 +3,10 @@ class Admin::GifsController < Admin::BaseController
     category = Category.find_or_create_by(name: params[:category])
     gif = CreateGif.call(category)
     if gif.save
-      redirect_to gifs_path
+      flash[:notice] = "#{category.name} gif created!"
     else
-      render :new
+      flash[:notice] = "Something went wrong. Let's try that again"
     end
+    redirect_to gifs_path
   end
 end
